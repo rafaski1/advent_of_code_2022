7fc8e8b5b9d1ca15eab138438853ad7b90e3dc27
rafaski1 <rav.skibinski@gmail.com>
2022-12-04 12:39:39 +0100

    day_04

--- day_04/main.py
+++ day_04/main.py
@@ -5,24 +5,31 @@ https://adventofcode.com/2022/day/4
 """
 
 with open(r"input.txt", "r") as file:
-    count_part1 = 0
+    count_part1_sol1 = 0
+    count_part1_sol2 = 0
     count_part2 = 0
     for line in file.readlines():
         line = [int(i) for i in re.findall(r"\d+", line)]
         if (line[0] <= line[2]) and (line[1] >= line[3]):
-            count_part1 += 1
+            count_part1_sol1 += 1
         elif (line[0] >= line[2]) and (line[1] <= line[3]):
-            count_part1 += 1
+            count_part1_sol1 += 1
+
+        # part 1 solution 2 with subsets
+        x = range(line[0], line[1] + 1)
+        y = range(line[2], line[3] + 1)
+        if set(x).issubset(set(y)):
+            count_part1_sol2 += 1
+        elif set(y).issubset(set(x)):
+            count_part1_sol2 += 1
 
         # part 2
-        x = range(line[0], line[1]+1)
-        y = range(line[2], line[3]+1)
-
         overlap = set(x) & set(y)
         if overlap:
             count_part2 += 1
 
-    print(count_part1)
+    print(count_part1_sol1)
+    print(count_part1_sol2)
     print(count_part2)
 
 
