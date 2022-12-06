0d7664575d308daaafae5f3ffd5e5d45c1d87428
rafaski1 <rav.skibinski@gmail.com>
2022-12-06 10:35:39 +0100

    day_05

--- day_05/main.py
+++ day_05/main.py
@@ -5,18 +5,22 @@ https://adventofcode.com/2022/day/5
 """
 
 stack = {
-    1: ["P", "V", "Z", "W", "D", "T"],
-    2: ["D", "J", "F", "V", "W", "S", "L"],
-    3: ["H", "B", "T", "V", "S", "L", "M", "Z"],
-    4: ["J", "S", "R"],
-    5: ["W", "L", "M", "F", "G", "B", "Z", "C"],
-    6: ["B", "G", "R", "Z", "H", "V", "W", "Q"],
-    7: ["N", "D", "B", "C", "P", "J", "V"],
-    8: ["Q", "B", "T", "P"],
-    9: ["C", "R", "Z", "G", "H"]
+    1: ['T', 'D', 'W', 'Z', 'V', 'P'],
+    2: ['L', 'S', 'W', 'V', 'F', 'J', 'D'],
+    3: ['Z', 'M', 'L', 'S', 'V', 'T', 'B', 'H'],
+    4: ['R', 'S', 'J'],
+    5: ['C', 'Z', 'B', 'G', 'F', 'M', 'L', 'W'],
+    6: ['Q', 'W', 'V', 'H', 'Z', 'R', 'G', 'B'],
+    7: ['V', 'J', 'P', 'C', 'B', 'D', 'N'],
+    8: ['P', 'T', 'B', 'Q'],
+    9: ['H', 'G', 'Z', 'R', 'C']
 }
 
-top_stack = ""
+stack_copy = stack.copy()
+
+top_stack_1 = ""
+top_stack_2 = ""
+
 with open(r"input.txt", "r") as file:
     for line in file.readlines()[10:]:
         line = [int(i) for i in re.findall(r"\d+", line)]
@@ -26,7 +30,13 @@ with open(r"input.txt", "r") as file:
             stack[to_stack].append(stack[from_stack][-1])
             stack[from_stack].pop()
 
-    for i in range(1, 10):
-        top_stack += stack.get(i)[-1]
+        # part 2
+        stack_copy[to_stack].extend(stack_copy[from_stack][-quantity:])
+        del stack_copy[from_stack][-quantity:]
 
-    print(top_stack)
+    for i in range(1, 10):
+        top_stack_1 += stack.get(i)[-1]
+        top_stack_2 += stack_copy.get(i)[-1]
+
+    print(top_stack_1)
+    print(top_stack_2)
